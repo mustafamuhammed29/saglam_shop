cdac72cfb3b9578e0da6d4d97d82a4944590de20
Mustafa Muhammed <mustafamuhammed665@gmail.com>
2022-03-19 15:37:12 +0300

    2

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:saglam_shop/Utilites/toolsutilites.dart';
 import 'package:saglam_shop/drawer/mydrawer.dart';
+import 'package:saglam_shop/pages/product.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,19 +15,31 @@ class _HomeState extends State<Home> {
       "pro_id": "1",
       "pro_name": "product1",
       "pro_desc": "productproductproduct1",
-      "cat image": "images/category/1.png"
+      "pro image": "images/product/1.jpg"
     },
     {
       "pro_id": "2",
       "pro_name": "product2",
       "pro_desc": "productproductproduct1",
-      "cat image": "images/category/2.png"
+      "pro image": "images/product/2.png"
     },
     {
       "pro_id": "3",
       "pro_name": "product3",
       "pro_desc": "productproductproduct1",
-      "cat image": "images/category/3.png"
+      "pro image": "images/product/2.png"
+    },
+    {
+      "pro_id": "3",
+      "pro_name": "product3",
+      "pro_desc": "productproductproduct1",
+      "pro image": "images/product/2.png"
+    },
+    {
+      "pro_id": "3",
+      "pro_name": "product3",
+      "pro_desc": "productproductproduct1",
+      "pro image": "images/product/2.png"
     },
   ];
   var myarr_category = [
@@ -102,7 +116,8 @@ class _HomeState extends State<Home> {
                             onTap: () {
                               _KeyDrawer.currentState.openEndDrawer();
                             },
-                            child: new Icon(Icons.menu)),
+                            child: new Icon(Icons.menu,
+                            size: 42,)),
                         Expanded(
                             child: new Container(
                           padding: EdgeInsets.only(left: 10, right: 10),
@@ -120,56 +135,97 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   new Container(
-                    width: MediaQuery.of(context).size.width / 1,
-                    height: 100,
+                    width: MediaQuery.of(context).size.width ,
+                    height: 110,
                     child: ListView.builder(
-                        itemCount: myarr_category.length,
                         scrollDirection: Axis.horizontal,
+                        itemCount: myarr_category.length,
                         itemBuilder: (BuildContext context, int index) {
                           return SingleCategory(
-                            cat_id: myarr_product[index]["cat_id"],
-                            cat_image: myarr_product[index]["cat_image"],
-                            cat_name: myarr_product[index]["cat_name"],
+                            cat_id: myarr_category[index]["cat_id"],
+                            cat_image: myarr_category[index]["cat_image"],
+                            cat_name: myarr_category[index]["cat_name"],
                           );
                         }),
                   ),
                   new Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        new Container(
-                          child: Text(" "),
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height / 4,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15.0),
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage("images/category/cat2.png"))),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        new Text(
-                          "pro name",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        new Text(
-                          "pro name name name name  name",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.grey),
-                        ),
-                      ],
-                    ),
+                    width: MediaQuery.of(context).size.width ,
+                    height: 200,
+                    child: ListView.builder(
+                        itemCount: myarr_product.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return SingleProduct(
+                            pro_id: myarr_product[index]["pro_id"],
+                            pro_image: myarr_product[index]["pro_image"],
+                            pro_name: myarr_product[index]["pro_name"],
+                            pro_desc: myarr_product[index]["pro_desc"],
+                          );
+                        }),
                   ),
-                ]))));
+
+                ]
+                )
+            ),
+
+              )
+
+
+          );
+
+
   }
 }
 
 
+class SingleProduct extends StatelessWidget {
+    final String pro_id;
+   final String pro_name;
+    final String pro_desc;
+    final String pro_image;
+
+
+    SingleProduct({ this.pro_id, this.pro_name, this.pro_desc, this.pro_image});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> ProductDetail()));
+      },
+      child: new Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/4 ,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(pro_image))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            new Text(
+              pro_name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+            new Text(
+              pro_desc,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class SingleCategory extends StatelessWidget {
   final String cat_id;
---
+++ lib/pages/product.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ProductDetail extends StatefulWidget {
+  @override
+  State<ProductDetail> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState extends State<ProductDetail> {
+  Widget HeaderBuild() {
+    return Container(
+      padding: EdgeInsets.all(15.5),
+      child: Row(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[100],
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset: Offset(0, 1))
+                  ],
+                  borderRadius: BorderRadius.circular(15)),
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.red,
+                  ))),
+          Expanded(child: Text(" ")),
+          Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[100],
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset: Offset(0, 1))
+                  ],
+                  borderRadius: BorderRadius.circular(15)),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.red,
+                  ))),
+        ],
+      ),
+    );
+  }
+
+  Widget imageProduct() {
+    return Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.red,
+                  spreadRadius: 1,
+                  blurRadius: 0,
+                  offset: Offset(0, 1))
+            ],
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50.0),
+              bottomRight: Radius.circular(50.0),
+            )),
+        child: Column(children: [
+          Image.asset("images/product/pro3.jpg"),
+          Padding(padding: EdgeInsets.only(top: 30.0)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey[100],
+                            spreadRadius: 1,
+                            blurRadius: 1,
+                            offset: Offset(0, 1))
+                      ],
+                      borderRadius: BorderRadius.circular(15)),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: FaIcon(
+                        FontAwesomeIcons.minus,
+                        color: Colors.white,
+                      ))),
+              Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    " 1 ",
+                    style: TextStyle(color: Colors.black87, fontSize: 25),
+                  )),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey[100],
+                            spreadRadius: 1,
+                            blurRadius: 1,
+                            offset: Offset(0, 1))
+                      ],
+                      borderRadius: BorderRadius.circular(15)),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: FaIcon(
+                        FontAwesomeIcons.plus,
+                        color: Colors.white,
+                      )))
+            ],
+          )
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          HeaderBuild(),
+          imageProduct(),
+          Container(
+            padding: EdgeInsets.all(35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Text(
+                  "Marka",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 15.0)),
+                new Row(
+                  children: [
+                    new Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+                    new Text("7"),
+                    Expanded(child: Text("")),
+                    new Icon(
+                      Icons.star,
+                      color: Colors.amberAccent,
+                    ),
+                    new Text("4.5"),
+                  ],
+                ),
+                new Text(
+                  " paragraphs consist of three parts: the topic sentence,paragraphs consist of three parts: the topic sentence body sentences, and the concluding o",
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              new Text(
+                " 5000TL ",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Expanded(child: Text("")),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(0, 1))
+                    ],
+                    borderRadius: BorderRadius.circular(40)),
+                margin: EdgeInsets.all(5),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Sepata Ekle ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15,color: Colors.white),
+                  
+                ),
+              ),
+              Container(
+                child: Icon(
+                  Icons.shopping_basket_outlined,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        
+        padding: EdgeInsets.only(left: 50, right: 30),
+        height: 65.0,
+        decoration: BoxDecoration(
+            color: Colors.red[300],
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.red,
+                  spreadRadius: 0,
+                  blurRadius: 0,
+                  offset: Offset(0, 3))
+            ],
+            borderRadius: BorderRadius.circular(50)),
+      ),
+    );
+  }
+}
